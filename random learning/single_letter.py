9cbaf4285ece95147f898b1827526971aed26eee
unknown <ajgorman84@gmail.com>
2021-07-12 00:15:39 -0400

    for work

--- random learning/single_letter.py
+++ random learning/single_letter.py
@@ -1,4 +1,10 @@
-def single_letter_count(word,letter):
-    return word.lower().count(letter.lower())
+# def single_letter_count(word,letter):
+#     return word.lower().count(letter.lower())
 
-print(single_letter_count(word = input("Word: "), letter = input("Letter: ")))
+# print(single_letter_count(word = input("Word: "), letter = input("Letter: ")))
+
+def ft(distance):
+    return f"{round(int(distance) * 3.28084, 2)} feet."
+
+distance = int(input("Provide a int in meters to calculate as feet: "))
+print(ft(distance))
