03c8a461a4a34de7d52e33ab10abd9e21337c498
unknown <ajgorman84@gmail.com>
2021-07-15 00:52:38 -0400

    afer work1

---
+++ random learning/function.py
@@ -0,0 +1,10 @@
+def function2(x,y):
+    return 2 * (x + y)
+
+# print(function2(int(input("enter number: ", "more"))))
+
+for l in range(1):
+    x = int(input("num1: "))
+    y = int(input("num1: "))
+
+print(function2(x,y))
