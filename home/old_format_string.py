73bc3b3143b7b899aa8b4f09e6908bfb60b2009c
unknown <ajgorman84@gmail.com>
2021-07-16 00:07:48 -0400

    afer work3

---
+++ home/old_format_string.py
@@ -0,0 +1,4 @@
+greeting = "hello"
+name = "Heisenberg"
+# greet_name = f"{}, {}.".format(greeting,name)
+print("Hello, {}. You are {}.".format(greeting,name))
---
+++ random learning/dict1.py
@@ -0,0 +1,11 @@
+# p = dict.fromkeys("aeiou", 0)
+
+# print(p)
+
+
+artist = {
+    "first": "Neil",
+    "last": "Young",
+}
+
+print(f"{artist['first']} {artist['last']}")
