cbe5a28602bda5e79f5e38afaf358b3e86af5059
unknown <ajgorman84@gmail.com>
2021-07-18 01:28:23 -0400

    home1

--- home/codingbat_list2.py
+++ home/codingbat_list2.py
@@ -17,15 +17,56 @@
 # print(big_diff([7, 2, 10, 9]))# → 8
 # print(big_diff([2, 10, 7, 2]))# → 8
 
-def count_hi(str):
-  count = 0
-  for i in range(len(str)-1):
-    if str[i:i+2] == 'hi':
-      count += 1
-  return count
+# def count_hi(str):
+#   count = 0
+#   for i in range(len(str)-1):
+#     if str[i:i+2] == 'hi':
+#       count += 1
+#   return count
 
-print(count_hi('abc hi ho'))
-print(count_hi('ABChi hi'))
-print(count_hi('hihi'))
-print(count_hi('Hi is no HI on ahI'))
-print(count_hi('hiho not HOHIhi'))
+# print(count_hi('abc hi ho'))
+# print(count_hi('ABChi hi'))
+# print(count_hi('hihi'))
+# print(count_hi('Hi is no HI on ahI'))
+# print(count_hi('hiho not HOHIhi'))
+
+
+
+# def sum13(nums):
+#   if nums == [] or nums == '':
+#     return 0
+#   if 13 not in nums:
+#     return sum(nums)
+#   while 13 in nums:
+#     del nums[nums.index(13):nums.index(13) + 2]
+#   return sum(nums)
+
+# print(sum13([1, 2, 2, 1]))# → 6
+# print(sum13([1, 1]))# → 2
+# print(sum13([1, 2, 2, 1, 13, 12]))# → 6
+# print(sum13([]))
+# print(sum13([13, 1, 2, 13, 2, 1, 13]))
+
+
+# WORKING ON THIS #
+# def sum67(nums):
+#   if nums == [] or nums == '':
+#     return 0
+#   if 6 not in nums:
+#     return sum(nums)
+#   while 6 in nums:
+#     del nums[nums.index(6):nums.index(7) + 1]
+#   return sum(nums)
+
+
+# print(sum67([1, 2, 2]))# → 5
+# print(sum67([1, 2, 2, 6, 99, 99, 7]))# → 5
+# print(sum67([1, 1, 6, 7, 2]))# → 4
+
+def has22(nums):
+  for num in nums:
+    return num
+
+print(has22([1, 2, 2]))# → True
+# print(has22([1, 2, 1, 2]))# → False
+# print(has22([2, 1, 2]))# → False
