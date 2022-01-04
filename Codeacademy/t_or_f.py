acf0f92643e5bd2d611669150d6471afb0024e92
agorman75 <ajgorman84@gmail.com>
2022-01-04 16:58:15 -0500

    update

--- Codeacademy/t_or_f.py
+++ Codeacademy/t_or_f.py
@@ -1,5 +1,5 @@
-print((4 <= 2 * 3) and (7 + 1 == 8))
-print(4 * 5 <= 21 - 1)
+# print((4 <= 2 * 3) and (7 + 1 == 8))
+# print(4 * 5 <= 21 - 1)
 
 # x = 5
  
@@ -13,3 +13,24 @@ print(4 * 5 <= 21 - 1)
 #   print("This might be printed.")
 
 # print((9 - 4) * 2 == 77 / 7 - 1)
+
+# orders = ["daisy", "buttercup", "snapdragon", "gardenia", "lily"]
+
+# orders2 = ["flower", "foot", "fake"]
+
+# # for n in orders2:
+# #     # orders.append(n)
+# #     orders
+# # orders.append(orders2)
+
+# print(orders + [2]["help"])
+
+def solution(number):
+    add = 0
+    for n in number:
+        add = 0
+        if n % 3 == 0 or n % 5 == 0:
+            add += n
+    return print(add)
+
+number = 1
