a3d0490bdcdb78829c99ffb54fe1cb9cf333933c
agorman75 <ajgorman84@gmail.com>
2022-01-04 15:12:46 -0500

    adding codeacademy

---
+++ Codeacademy/t_or_f.py
@@ -0,0 +1,15 @@
+print((4 <= 2 * 3) and (7 + 1 == 8))
+print(4 * 5 <= 21 - 1)
+
+# x = 5
+ 
+# if x <= 2:
+#   print("This is printed")
+# if x <= 4:
+#   print("This is also printed")
+# if x <= 6:
+#   print("Is this printed?")
+# if x <= 8:
+#   print("This might be printed.")
+
+# print((9 - 4) * 2 == 77 / 7 - 1)
